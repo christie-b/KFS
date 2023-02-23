01e4c0d34969a52441bd9d824cdaf57e0c71ae5d
Pierre CHAMBON <pierre.chambon@gard.fr>
2023-02-23 14:59:21 +0100

    .

--- srcs/kernel/handler/isr.c
+++ srcs/kernel/handler/isr.c
@@ -1,5 +1,7 @@
 #include "handler.h"
 
+extern t_interrupt_handler_func_ptr interrupt_handlers[256];
+
 void isr_handler(t_registers regs)
 {
     if (interrupt_handlers[regs.int_no] != 0)
@@ -10,49 +12,49 @@ void isr_handler(t_registers regs)
     switch (regs.int_no)
     {
         case 0:
-            k_print(KERR "Division by Zero\n");
+            k_print("Division by Zero\n");
             break;
         case 1:
-            k_print(KERR "Debug\n");
+            k_print("Debug\n");
             break;
         case 2:
-            k_print(KERR "Non Maskable Interrupt\n");
+            k_print("Non Maskable Interrupt\n");
             break;
         case 3:
-            k_print(KERR "Breakpoint\n");
+            k_print("Breakpoint\n");
             break;
         case 4:
-            k_print(KERR "Overflow\n");
+            k_print("Overflow\n");
             break;
         case 5:
-            k_print(KERR "Bound Range Exceeded\n");
+            k_print("Bound Range Exceeded\n");
             break;
         case 6:
-            k_print(KERR "Invalid Opcode\n");
+            k_print("Invalid Opcode\n");
             break;
         case 7:
-            k_print(KERR "Device Not Available\n");
+            k_print("Device Not Available\n");
             break;
         case 8:
-            k_print(KERR "Double Fault\n");
+            k_print("Double Fault\n");
             break;
         case 10:
-            k_print(KERR "Invalid TSS\n");
+            k_print("Invalid TSS\n");
             break;
         case 11:
-            k_print(KERR "Segment Not Present\n");
+            k_print("Segment Not Present\n");
             break;
         case 12:
-            k_print(KERR "Stack-Segment Fault\n");
+            k_print("Stack-Segment Fault\n");
             break;
         case 13:
-            k_print(KERR "General Protection Fault\n");
+            k_print("General Protection Fault\n");
             break;
         case 14:
-            k_print(KERR "Page Fault\n");
+            k_print("Page Fault\n");
             break;
         default:
-            k_print(KERR "ISR code = %d\n", regs.int_no);
+            k_print("ISR code = %d\n", regs.int_no);
             break;
     }
 }
