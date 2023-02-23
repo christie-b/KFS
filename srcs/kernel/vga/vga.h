e2757adb060b7df3c67b93401b794e9628ef7bf2
Pierre CHAMBON <pierre.chambon@gard.fr>
2023-02-23 11:21:48 +0100

    .

--- srcs/kernel/vga/vga.h
+++ srcs/kernel/vga/vga.h
@@ -6,12 +6,16 @@
 extern unsigned short* 	terminal_buffer;
 extern unsigned int 	vga_index;
 
-void            clear_screen();
-void            move_right();
+void            newline();
 void            move_left();
+void            scroll_up();
+void            move_right();
+void            clear_screen();
+void            clear_line(int line);
+void            clear_previous_char();
 void            move_cursor_left(int nb);
 void            move_cursor_right(int nb);
-void            clear_previous_char();
+void            copy_line(int dst, int src);
 void            set_vga_index(unsigned int index);
 
 #endif
